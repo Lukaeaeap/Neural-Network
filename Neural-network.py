c2e6a9ba70a26c6748035b5118a3fe7b281f0842
Luuk <lukasrobinhoekstra@gmail.com>
2022-04-01 11:09:55 +0200

    Fixed path isues

--- Neural-network.py
+++ Neural-network.py
@@ -1137,8 +1137,11 @@ mnist_labels = {
 # set parameters load and model load to false to train a new set of parameters and make a model
 # adjust one of the two to either load a model or a set of parameters
 # use this function for debugging and creating a new model
-MNIST_path = "C:/Users/lukae/Documents/Code/Neural-network-GITHUB_REPO/input/MNIST/"
-trained = True
+
+# You can tweek the variables of the Neural Network in the create_NN function which is in the Model class
+
+MNIST_path = "input/MNIST/"
+trained = False
 if trained is False:
     Model.create_NN('digit_mnist', parms_load=False, model_load=False, folder_path=MNIST_path)
 
